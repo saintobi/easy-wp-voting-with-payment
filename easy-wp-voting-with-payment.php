1b5304eda0fe15a94cdb56624f86241afa5c4d44
Mujhtech <mujeeb.muhideen@gmail.com>
2020-08-11 08:45:12 +0100

    add easy wp voting with system file

---
+++ easy-wp-voting-with-payment.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Easy_WP_Voting_With_Payment
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Easy WP Voting With Payment
+Plugin URI: https://github.com/Mujhtech/easy-wp-voting-with-payment
+Description: This plugin is an easy voting system plugin integrated with payment system
+Author: Mujhtech Mujeeb Muhideen
+Version: 1.0.0
+Author URI: https://github.com/Mujhtech/
+*/
