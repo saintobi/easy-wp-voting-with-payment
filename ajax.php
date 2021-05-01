48446fcf8860a68fd94ac4e261ecaa6d27b6a3d0
Mujhtech <mujeeb.muhideen@gmail.com>
2021-05-02 00:31:39 +0100

    ....

--- ajax.php
+++ ajax.php
@@ -7,18 +7,18 @@
     ========================
 */
 
-    add_action('wp_ajax_nopriv_ewvwp_form_ajax', 'ewvwp_ajax');
-    add_action('wp_ajax_ewvwp_form_ajax', 'ewvwp_ajax');
+add_action('wp_ajax_nopriv_ewvwp_form_ajax', 'ewvwp_ajax');
+add_action('wp_ajax_ewvwp_form_ajax', 'ewvwp_ajax');
 
-    function ewvwp_ajax()
-    {
-      $quantity = intval($_POST['quantity']);
-      $userID = intval($_POST['userID']);
-      $reference = sanitize_text_field($_POST['reference']);
-      $email = sanitize_email($_POST['email']);
+function ewvwp_ajax()
+{
+    $quantity = intval($_POST['quantity']);
+    $userID = intval($_POST['userID']);
+    $reference = sanitize_text_field($_POST['reference']);
+    $email = sanitize_email($_POST['email']);
 
-      //The parameter after verify/ is the transaction reference to be verified
-      $url = 'https://api.paystack.co/transaction/verify/'.$reference;
+    //The parameter after verify/ is the transaction reference to be verified
+    $url = 'https://api.paystack.co/transaction/verify/'.$reference;
 
 
       $headers = array(
@@ -53,45 +53,46 @@
                 'success' => false,
                 'message' => "Candidate not found"
               );
+
               return wp_send_json( $result );
 
             } else {
+        
+                $vote = get_post_meta($userID, "_ewvwp_vote_value_key", true);
 
-              $vote = get_post_meta($userID, "_ewvwp_vote_value_key", true);
+                $total = $vote + $quantity;
+                update_post_meta( $userID, '_ewvwp_vote_value_key', $total );
 
-              $total = $vote + $quantity;
-              update_post_meta( $userID, '_ewvwp_vote_value_key', $total );
-
-              $result = array(
-                'success' => true,
-                'message' => "Thanks for voting"
-              );
+                $result = array(
+                  'success' => true,
+                  'message' => "Thanks for voting"
+                );
               
-              $from = "mujhtech@gmail.com";
-              $title = "Easy Wp Voting With Payment";
-              $headers = "From: $title <$from> \r\n";
-              $headers .= "Reply-To: $title <$from> \r\n";
-              $headers .= "MIME-Version: 1.0\r\n";
-              $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+                //$from = "mujhtech@gmail.com";
+                //$title = "Easy Wp Voting With Payment";
+                //$headers = "From: $title <$from> \r\n";
+                //$headers .= "Reply-To: $title <$from> \r\n";
+                //$headers .= "MIME-Version: 1.0\r\n";
+                //$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-              $message = "Thanks for voting, we appreciate your votes";
+                //$message = "Thanks for voting, we appreciate your votes";
 
-              wp_mail($to, $subject, $message, $headers);
-
-              return wp_send_json( $result );
+                //wp_mail($to, $subject, $message, $headers);
+                
+                return wp_send_json( $result );
             }
 
 
 
-            return wp_send_json($paystack_response);
+          return wp_send_json($paystack_response);
 
-          } else {
+        } else {
 
-            return wp_send_json(array('success' => false, 'status' => 500, 'message' => 'Payment failed'));
+          return wp_send_json(array('success' => false, 'status' => 500, 'message' => 'Payment failed'));
 
-          }
         }
-
-
-        die();
       }
+
+
+    die();
+}
--- easy-wp-voting-with-payment.php
+++ easy-wp-voting-with-payment.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Easy_WP_Voting_With_Payment
- * @version 2.1.0
+ * @version 2.2.0
  */
 /*
 Plugin Name: Easy WP Voting With Payment
 Plugin URI: https://github.com/Mujhtech/easy-wp-voting-with-payment
 Description: Easy WP Voting With Payment allows you to create a simple voting system with payment method
 Author: Mujhtech Mujeeb Muhideen
-Version: 2.1.0
+Version: 2.2.0
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 Author URI: https://github.com/Mujhtech/
@@ -75,7 +75,7 @@ function ewvwp_scripts(){
     wp_enqueue_style( 'ewvwp-owl-carousel-css', plugin_dir_url(__FILE__) . 'assets/css/style.css', array(), '1.0.0', 'all' );
 
     wp_enqueue_style( 'ewvwp-sweetalert-css', plugin_dir_url(__FILE__) . 'assets/css/sweetalert.css', array(), '1.0.0', 'all' );
-    
+
     wp_enqueue_script( 'ewvwp-paystack-js', 'https://js.paystack.co/v1/inline.js', array(), '1.0' );
     
     wp_enqueue_script( 'ewvwp-jquery' , plugin_dir_url(__FILE__) . 'assets/js/jquery.min.js', false, '1.11.3', true );
@@ -83,7 +83,7 @@ function ewvwp_scripts(){
     wp_enqueue_script( 'ewvwp-js', plugin_dir_url(__FILE__) . 'assets/js/script.js', array('ewvwp-jquery'), '1.0.0', true );
 
     wp_enqueue_script( 'ewvwp-sweetalert-js', plugin_dir_url(__FILE__) . 'assets/js/sweetalert.js', false, '1.0', true );
-    
+
 }
 add_action( 'wp_enqueue_scripts', 'ewvwp_scripts' );
 
